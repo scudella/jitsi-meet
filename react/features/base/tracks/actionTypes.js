6e0644893b3cf235ab5abe30acbaf5fee5ae8978
paweldomas <pawel.domas@jitsi.org>
2017-11-09 15:59:31 -0600

    fix(base/tracks): handle GUM in progress
    This commit adds extra actions/Redux state to be able to deal with
    the GUM operation being in progress. There will be early local track
    stub in the Redux state for any a local track for which GUM has been
    called, but not completed yet.
    
    Local track is considered valid only after TRACK_ADDED event when it
    will have JitsiLocalTrack instance set.

--- react/features/base/tracks/actionTypes.js
+++ react/features/base/tracks/actionTypes.js
@@ -10,6 +10,46 @@
 export const TRACK_ADDED = Symbol('TRACK_ADDED');
 
 /**
+ * Action triggered when a local track starts being created through the WebRTC
+ * getUserMedia call. It will include extra 'gumProcess' field which is
+ * a Promise with extra 'cancel' method which can be used to cancel the process.
+ * Canceling will result in disposing any JitsiLocalTrack returned by the GUM
+ * callback. There will be TRACK_CREATE_CANCELED event instead of track
+ * added/gum failed events.
+ *
+ * @type {
+ *      type: TRACK_BEING_CREATED
+ *      track: {
+ *          local: true,
+ *          mediaType: MEDIA_TYPE,
+ *          gumProcess: Promise with cancel() method to abort
+ *      }
+ * }
+ */
+export const TRACK_BEING_CREATED = Symbol('TRACK_BEING_CREATED');
+
+/**
+ * Action sent when canceled GUM process completes either successfully or with
+ * an error (error is ignored and track is immediately disposed if created).
+ *
+ * @type {
+ *      type: TRACK_CREATE_CANCELED,
+ *      trackType: MEDIA_TYPE
+ * }
+ */
+export const TRACK_CREATE_CANCELED = Symbol('TRACK_CREATE_CANCELED');
+
+/**
+ * Action sent when GUM fails with an error other than permission denied.
+ *
+ * @type {
+ *      type: TRACK_CREATE_ERROR,
+ *      trackType: MEDIA_TYPE
+ * }
+ */
+export const TRACK_CREATE_ERROR = Symbol('TRACK_CREATE_ERROR');
+
+/**
  * Action for when a track cannot be created because permissions have not been
  * granted.
  *
